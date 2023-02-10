41e254036cac4adfc05d83f232ee3e41dc27e7e2
Raul Hernandez <raulhsj@gmail.com>
2023-02-10 12:08:29 +0100

    ref: add and exposing properties to dto

--- src/modules/user/dto/read-user.dto.ts
+++ src/modules/user/dto/read-user.dto.ts
@@ -1,17 +1,27 @@
 import { IsEmail, IsNumber, IsString } from 'class-validator';
 import { ReadUserDetailsDto } from './read-user-details.dto';
-import { Type } from 'class-transformer';
+import { Exclude, Expose, Type } from 'class-transformer';
+import { ReadRoleDto } from 'src/modules/role/dtos';
 
+@Exclude()
 export class ReadUserDto {
+  @Expose()
   @IsNumber()
   readonly id: number;
 
+  @Expose()
   @IsEmail()
   readonly email: string;
 
+  @Expose()
   @IsString()
   readonly username: string;
 
+  @Expose()
   @Type((type) => ReadUserDetailsDto) // automatic cast to ReadUserDetailsDto
   readonly details: ReadUserDetailsDto;
+
+  @Expose()
+  @Type(() => ReadRoleDto)
+  readonly roles: ReadRoleDto[];
 }
